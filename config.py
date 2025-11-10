81b1f56cb8afbaf498141bda9272293d16e5d9ce
fackyhindi03 <fackyhindidonghua03@gmail.com>
2025-11-10 10:08:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
 
-    BOT_TOKEN = "7484678112:AAGuQelyqge8Jm-zyzhUGs1y4YbSK9oxR7E"
+    BOT_TOKEN = "7260809129:AAGmpo4xGrXVk_7emrW9hCQK3vL9dXhzq3A"
     APP_ID = 27999679
     API_HASH = "f553398ca957b9c92bcb672b05557038"
 
