37c494e467dc5f784eda9c1e3d8e86a6da9fdb6f
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-11-10 10:02:18 +0530

    Update muxbot.py

--- muxbot.py
+++ muxbot.py
@@ -4,9 +4,31 @@ from config import Config
 from helper_func.dbhelper import Database as Db
 from plugins.muxer import queue_worker
 
-logging.basicConfig(level=logging.DEBUG,
-                    format="%(asctime)s - %(name)s - %(message)s")
-logging.getLogger('pyrogram').setLevel(logging.WARNING)
+os.makedirs("logs", exist_ok=True)
+
+log_fmt = "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+from logging.handlers import RotatingFileHandler
+fh = RotatingFileHandler("logs/bot.log", maxBytes=5_000_000, backupCount=3, encoding="utf-8")
+fh.setFormatter(logging.Formatter(log_fmt))
+fh.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter(log_fmt))
+ch.setLevel(logging.INFO)
+
+logger.handlers.clear()
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
+
+def _uncaught(exc_type, exc, tb):
+    logging.getLogger("bot").error("Uncaught exception", exc_info=(exc_type, exc, tb))
+sys.excepthook = _uncaught
 
 db = Db().setup()
 if not os.path.isdir(Config.DOWNLOAD_DIR):
